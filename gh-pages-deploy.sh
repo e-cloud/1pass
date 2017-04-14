7cf6b516515c5aef9375464965b32253500d317c
e-cloud <saintscott119@gmail.com>
2017-04-14 15:44:57 +0800

    chore(build): avoid using global gulp

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-gulp build
+npm run build
 
 git branch -D gh-pages
 git add -f dist/** && \
  git commit -m "chore(release): gh-pages release" && \
- git subtree split --prefix dist -b gh-pages && \
+ git subtree split --prefix=dist -b gh-pages && \
  git push origin gh-pages:gh-pages --force-with-lease && \
  git branch -D gh-pages && \
  git reset --hard HEAD~1
