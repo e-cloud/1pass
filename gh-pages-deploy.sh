677d81b50323653a3d83263acc938439269c7df3
e-cloud <saintscott119@gmail.com>
2018-03-03 16:22:59 +0800

    chore: apply new approach to releaes gh-pages

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -2,10 +2,11 @@
 
 npm run build
 
-git branch -D gh-pages
-git add -f dist/** && \
- git commit -m "chore(release): gh-pages release" && \
- git subtree split --prefix=dist -b gh-pages && \
- git push origin gh-pages:gh-pages --force-with-lease && \
- git branch -D gh-pages && \
- git reset --hard HEAD~1
+currentBranch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+
+git checkout gh-pages
+  mv dist/** ./ && \
+  git add -A && \
+  git commit -m "chore(release): gh-pages release" && \
+  git push origin gh-pages:gh-pages && \
+  git checkout $currentBranch
