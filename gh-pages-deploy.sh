31d6c43a3cadf67d56e72575732f365a01fa267c
e-cloud <saintscott119@gmail.com>
2018-05-26 18:58:46 +0800

    chore: improve gh-deploy

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -5,7 +5,7 @@ npm run build
 currentBranch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 
 git checkout gh-pages
-  mv -fu dist/** ./ && \
+  yes | cp -rf dist/** ./ && \
   git add -A && \
   git commit -m "chore(release): gh-pages release" && \
   git push origin gh-pages:gh-pages && \
